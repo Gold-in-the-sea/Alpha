d2e79ad46ffa14b125a2c5df9fa6512475fc0407
wangtao <1049295955@qq.com>
2020-03-31 20:03:18 +0800

    假意提交

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import {HashRouter,Route} from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import Administrator from './pages/Administrator'
 import loadalbe from './utils/loadable'
 
-const Login = loadalbe(()=>import('./pages/Login'))
-const Admin = loadalbe(()=>import('./pages/Admin'))
-class App extends React.Component{
+const Login = loadalbe(() => import('./pages/Login'))
+const Admin = loadalbe(() => import('./pages/Admin'))
 
-  render(){
-    return(
-        <HashRouter>
-          <Route path='/login' component={Login}></Route>
-          <Route path='/admin' render={()=>{
-            return(
-              <Admin>
-                <Route path='/admin/administrator' component={Administrator}></Route>
-              </Admin>
-            )
-          }}></Route>
-        </HashRouter>
+class App extends React.Component {
+
+  render() {
+    return (
+      <HashRouter>
+        <Route path='/login' component={Login}></Route>
+        
+
+        <Route path='/admin' render={() => {
+          return (
+            <Admin>
+              <Route path='/admin/administrator' component={Administrator}></Route>
+            </Admin>
+          )
+        }}></Route>
+      </HashRouter>
     )
   }
 }
