de23b70ef44bbc231a58df3ec31da04fb17dc786
wangtao <1049295955@qq.com>
2020-04-02 13:41:17 +0800

    花里胡哨

--- src/api/goods.js
+++ src/api/goods.js
@@ -2,9 +2,9 @@ import axios from '../utils/axios'
 class Goods {
     list(page=1,pageSize=2 ){
         // http://localhost:3000/shop/getInfos
-        let url ='hema/goods/'
+        let url ='/hema/shop/getInfos'
         return axios.post(url,{page,pageSize})
     }
-}
+} 
 
 export default new Goods()
--- src/pages/Goods/GoodsList/index.js
+++ src/pages/Goods/GoodsList/index.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, message,  Table } from 'antd';
 import goodsApi from '../../../api/goods'
 import style from './index.module.less';
 class Goods extends Component {
     state = {
         page: 1,
-        pageSize: 2
+        pageSize: 2,
+        list: [],
+        allcount: 0,
+        columns:[
+            {title:'_id',dataIndex:'_id',key:'_id'},
+            {title:'价格',dataIndex:'price',key:'price'},
+            {title:'名称',dataIndex:'name',key:'name'},
+            {title:'图片',dataIndex:'img',key:'img'},
+            {title:'描述',dataIndex:'desc',key:'desc'},
+            {title:'种类',dataIndex:'shopType',key:'shopType'},
+        
+        ]
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getListData()
     }
     getListData = async () => {
         let { page, pageSize } = this.state
-        let result = await goodsApi.list(page, pageSize)
-        console.log(result)
+        let { code, msg, list, allCount } = await goodsApi.list()
+        if (code !== 0) { return message.error(msg) }
+        this.setState({ list, allCount })
     }
     render() {
+        let {list,columns}=this.state
+        console.log({list},this.state)
         return (
             <div className={style.box}>
                 <Card title='商品列表' className={style.card}>
-
+                    <Table columns={columns} dataSource={list} rowKey='_id'></Table>
 
                 </Card>
             </div>
